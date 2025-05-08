6fb8ce5bddb0f4ac477d405aabb2a9c94d0ad7fd
PRATEEK SAHU <43596921+prateeksahu147@users.noreply.github.com>
2025-05-08 20:48:57 +0530

    neural roots business

---
+++ app/src/layout/ContactLayout.js
@@ -0,0 +1,45 @@
+function ContactLayout() {
+  return (
+    
+    //<!-- ======= Contact Section ======= -->
+    <section id="contact" className="contact">
+      <div className="container" data-aos="fade-up">
+
+        <div className="section-title">
+          <h2>Contact</h2>
+          </div>
+          <div className="col-lg-15 d-flex align-items-stretch">
+            <div className="info">
+              <div className="address">
+                <i className="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>#2245, 24th Main Road, HSR Sector 1, Bangaluru, KA, India</p>
+              </div>
+
+              <div className="email">
+                <i className="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>info@neuralroots.in</p>
+              </div>
+
+              <div className="phone">
+                <i className="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+91-7748864147</p>
+              </div>
+
+              </div>
+
+
+        </div>
+
+      </div>
+    </section>
+    //<!-- End Contact Section -->
+
+  );
+}
+
+export default ContactLayout;
+
+
